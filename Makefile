83e060ce85ac844ea63cda4521b10a348d3c5722
Riccardo Carlesso <ricc@ricc-macbookpro.corp.google.com>
2020-09-03 10:12:49 +0200

    now suports both BQ and DS

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 
 
+help:
+	cat Makefile | egrep -v '^$$'
+
 config:
 	@echo You need to copy config.dist into config and edit away.
 	exit 42
@@ -12,7 +15,11 @@ install: config-with-comment
 	@echo TODO apply Jinja templates with config 
 
 run: config-with-comment
-	ENV=prod ruby substitute_variables.rb
+	ENV=prod ruby substitute_variables.rb api
+
+run-bq-cli:
+	@echo Executing BQ queries from CLI to make sure your config is correct.
+	ENV=prod ruby substitute_variables.rb bqcli
 
 test: config-with-comment
 	ENV=test ruby substitute_variables.rb
--- substitute_variables.rb
+++ substitute_variables.rb
@@ -5,6 +5,11 @@ require 'erb'
 require 'erb'
 require 'ostruct'
 
+#(collection, *args)
+def println(*args)
+  print args #.extract_options!
+  puts ''
+end
 # needed to convert Hash into Binding :/ https://stackoverflow.com/questions/8954706/render-an-erb-template-with-values-from-a-hash
 class ErbalT < OpenStruct
   def render(template)
@@ -14,7 +19,7 @@ end
 
 #from string import Template
 $debug = true
-$env = 'test'
+#$env = 'test'
 
 
 def parse_vars_from_config(file = 'config')
@@ -38,14 +43,30 @@ def subtitute_file(filename, parameters=[])
     print "Rendered Template: #{output}" 
 end
 
+def process_bigquery_and_execute_from_cli()
+  p "Execute subtitute_file() with config, then pipe query into 'bq' command."
+
+end
+def process_config_and_invoke_datastudio_apis_to_provision_brandnew_dashboard()
+  p "TODO(ricc): Create a datastudio (also ensure idempotency so i can onlyt create a single DS per 10 executions.."
+  exit 41
+end
 
 def main
-    subtitute_file("test", 
-        %w( project_id dataset_name )) if $env == 'test'
-    subtitute_file("gcp-bq-audit", 
-        %w( project_id dataset_name )) if $env == 'prod'
-
-
+    args = ARGV
+    first_argument = ARGV[0] rescue nil
+    print "ENV:\t#{ ENV["ENV"]}\n"
+    $env =  ENV["ENV"] rescue 'test'
+    print "ARGV:\t#{args}\n" 
+    subtitute_file("test",         %w( project_id dataset_name )) if $env == 'test'
+    subtitute_file("gcp-bq-audit", %w( project_id dataset_name )) if $env == 'prod'
+    if first_argument == 'bqcli'
+      process_bigquery_and_execute_from_cli()
+    elsif first_argument == 'bqcli'
+      process_config_and_invoke_datastudio_apis_to_provision_brandnew_dashboard()
+    else 
+      print "Unknown command. first_argument='#{first_argument}'"
+    end
 end
 
 
